5d7ce245e15d485c71b881092ac1a622b93ab331
Abdhullah Al Noman <bdbrand58@gmail.com>
2025-11-16 01:45:36 +0600

    Optimize event listeners for login and popup actions
    Refactor event listeners to manage active classes more efficiently.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup= document.querySelector('.btnlogin-popup');
-const iconClose= document.querySelector('.icon-close');
-
+const btnPopup = document.querySelector('.btnlogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -13,11 +12,14 @@ loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
 
+
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
+    wrapper.classList.remove('active'); 
 });
 
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
 });
